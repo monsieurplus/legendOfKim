d73985f3cb607de23d855352fd002d3cfb4c629a
Adrien MAS <adrien.mas@airliquide.com>
2019-12-06 21:00:14 +0100

    Added the splash screen animation

---
+++ Assets/Scripts/SplashController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashController : MonoBehaviour
+{
+    public GameObject startMessage;
+    private bool startEnabled = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && startEnabled) {
+            SceneManager.LoadScene("Level01");
+        }
+    }
+
+    public void ReadyToStart() {
+        startMessage.SetActive(true);
+        startEnabled = true;
+    }
+}
