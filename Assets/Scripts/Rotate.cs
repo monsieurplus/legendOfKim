75c80e4ff56083961ded489343604b60850dcb62
Adrien MAS <adrien.mas@airliquide.com>
2019-12-15 00:50:55 +0100

    Added iLab scenery

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float speedX;
+    public float speedY;
+    public float speedZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(
+            speedX * Time.deltaTime, 
+            speedY * Time.deltaTime, 
+            speedZ * Time.deltaTime
+        );
+    }
+}
